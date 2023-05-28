7fec827f470e3c4d5130ffa306d5df4013f236b1
Adam Soutar <adam@overflo.me>
2023-05-28 23:16:02 +0100

    Implement executing the shell program and pulling stdout from it

---
+++ src/constants.rs
@@ -0,0 +1,14 @@
+use std::ffi::c_int;
+
+// Eg. on a ProMotion machine, change this to 120
+pub const TARGET_FRAMERATE: usize = 60;
+// This is subtracted from our poll allowance so we don't spend our entire frame budget on polling
+pub const RENDER_ALLOWANCE_MS: c_int = 3;
+// The maximum amount of time per-frame that we will wait for new bytes from the shell program.
+// This is so that we actually end up drawing and responding to user input instead of waiting
+// forever on a stalled program.
+pub const FD_POLL_TIMEOUT_MS: c_int = 1000 / TARGET_FRAMERATE as c_int - RENDER_ALLOWANCE_MS;
+// The amount of data we'll ask the child program file descriptor for at a time.
+// Essentially how many character chunks we're confident we can draw in one go without
+// stalling.
+pub const FD_BUFFER_SIZE_BYTES: usize = 4096;
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,11 @@
-use nix::{sys::wait::waitpid,unistd::{fork, ForkResult, write}};
+#[macro_use]
+extern crate nix;
+
+mod constants;
+mod process;
 
 fn main() {
-  let mut num = 2;
-  num += 1;
-  match unsafe{fork()} {
-     Ok(ForkResult::Parent { child, .. }) => {
-         println!("Continuing execution in parent process, new child has pid: {}", child);
-         waitpid(child, None).unwrap();
-     }
-     Ok(ForkResult::Child) => {
-         // Unsafe to use `println!` (or `unwrap`) here. See Safety.
-         write(libc::STDOUT_FILENO, "I'm a new child process\n".as_bytes()).ok();
-         unsafe { libc::_exit(0) };
-     }
-     Err(_) => println!("Fork failed"),
-  }
-  println!("num is {}", num);
+    unsafe {
+        process::fork_and_execute_shell();
+    }
 }
